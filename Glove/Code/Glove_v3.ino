279b7015a5156b486a5e4d8d8c60ebdd01f897d3
Mohanad <abualhan45@gmail.com>
2022-04-10 14:19:22 +0200

    fixed some issues + updated schematic

--- Glove/Code/Glove_v3.ino
+++ Glove/Code/Glove_v3.ino
@@ -1,7 +1,3 @@
-//****  IMPORTANT  ******
-// Change the pin numbers according to the new schematic (radio pins + analog pins)
-
-
 // Don't forget to download the libraries before including them
 #include <SPI.h>
 #include <nRF24L01.h>
@@ -11,7 +7,7 @@
 
 /*Create a unique pipe out. The receiver has to 
   wear the same unique code*/
-  
+
 const uint64_t pipeOut = 0xE8E8F0F0E1LL; //IMPORTANT: The same as in the receiver
 
 RF24 radio(9, 10); // nRF24l01 (CE, CSN)
@@ -31,15 +27,15 @@ int tg = 1; // Throttle gain
 int yg = 1; // Yaw gain
 
 // Set threshold values
-int throttle_up_threshold = 100;
-int throttle_down_threshold = 50;
-int yaw_up_threshold = 100;
-int yaw_down_threshold = 50;
-
+int throttle_up_threshold = 800;
+int throttle_down_threshold = 200;
+int yaw_up_threshold = 200;
+int yaw_down_threshold = 200;
 
 // The sizeof this struct should not exceed 32 bytes
 // This gives us up to 32 8 bits channals
-struct MyData {
+struct MyData
+{
   byte throttle;
   byte yaw;
   byte pitch;
@@ -50,12 +46,12 @@ struct MyData {
 
 MyData data;
 
-void resetData() 
+void resetData()
 {
   //This are the start values of each channal
   // Throttle is 0 in order to stop the motors
   //127 is the middle value of the 10ADC.
-    
+
   data.throttle = 0;
   data.yaw = 127;
   data.pitch = 127;
@@ -66,8 +62,9 @@ void resetData()
 
 void setup()
 {
-  Serial.begin(9600);
-  
+  // Uncomment this line if you want to get the IMU error values
+  //Serial.begin(9600);
+
   // Initialize interface to the MPU6050
   initialize_MPU6050();
   // Call this function if you need to get the IMU error values for your module
@@ -86,62 +83,99 @@ void setup()
 void loop()
 {
 
-  // if index finger is raised, throttle goes up
-  if (analogRead(A0) > throttle_up_threshold) {
-    data.throttle += tg;
-    data.throttle = constrain(data.throttle, 0, 255);
-  } else if ( analogRead(A0) < throttle_down_threshold) {
-    // if index finger is pinched, throttle goes down
-    data.throttle -= tg;
-    data.throttle = constrain(data.throttle, 0, 255);
+  // if index finger is bent, throttle goes up
+  if (analogRead(A0) > throttle_up_threshold)
+  {
+    if (data.throttle + tg < 255)
+    {
+      data.throttle += tg;
+      data.throttle = constrain(data.throttle, 0, 255);
+    }
+    else
+    {
+      data.throttle = 255;
+    }
+  }
+  else if (analogRead(A0) < throttle_down_threshold)
+  {
+    // if index finger is bent, throttle goes down
+    if (data.throttle - tg > 0)
+    {
+      data.throttle -= tg;
+      data.throttle = constrain(data.throttle, 0, 255);
+    }
+    else
+    {
+      data.throttle = 0;
+    }
   }
 
-  // if pinky finger is pinched, yaw clockwise
-  if (analogRead(A1) < yaw_up_threshold) {
-    data.yaw += yg;
-    data.yaw = constrain(data.yaw, 0, 255)
-  } else if (analogRead(A2) < yaw_down_threshold) {
-    // if ring finger is pinched, yaw counter clockwise
-    data.yaw -= yg;
-    data.yaw = constrain(data.yaw, 0, 255)
+  // if little finger is bent, yaw clockwise
+  if (analogRead(A1) < yaw_up_threshold)
+  {
+    if (data.yaw + yg < 255)
+    {
+      data.yaw += yg;
+      data.yaw = constrain(data.yaw, 0, 255);
+    }
+    else
+    {
+      data.yaw = 255;
+    }
   }
-  
+  else if (analogRead(A2) < yaw_down_threshold)
+  {
+    // if ring (or thumb?) finger is bent, yaw counter clockwise
+    if (data.yaw + yg > 0)
+    {
+      data.yaw -= yg;
+      data.yaw = constrain(data.yaw, 0, 255);
+    }
+    else
+    {
+      data.yaw = 0;
+    }
+  }
+
   // Calculate pitch and roll values from MPU6050
   read_IMU();
 
   radio.write(&data, sizeof(MyData));
 }
 
-void initialize_MPU6050() {
-  Wire.begin();                      // Initialize comunication
-  Wire.beginTransmission(MPU);       // Start communication with MPU6050 // MPU=0x68
-  Wire.write(0x6B);                  // Talk to the register 6B
-  Wire.write(0x00);                  // Make reset - place a 0 into the 6B register
-  Wire.endTransmission(true);        //end the transmission
+void initialize_MPU6050()
+{
+  Wire.begin();                // Initialize comunication
+  Wire.beginTransmission(MPU); // Start communication with MPU6050 // MPU=0x68
+  Wire.write(0x6B);            // Talk to the register 6B
+  Wire.write(0x00);            // Make reset - place a 0 into the 6B register
+  Wire.endTransmission(true);  //end the transmission
   // Configure Accelerometer
   Wire.beginTransmission(MPU);
-  Wire.write(0x1C);                  //Talk to the ACCEL_CONFIG register
-  Wire.write(0x10);                  //Set the register bits as 00010000 (+/- 8g full scale range)
+  Wire.write(0x1C); //Talk to the ACCEL_CONFIG register
+  Wire.write(0x10); //Set the register bits as 00010000 (+/- 8g full scale range)
   Wire.endTransmission(true);
   // Configure Gyro
   Wire.beginTransmission(MPU);
-  Wire.write(0x1B);                   // Talk to the GYRO_CONFIG register (1B hex)
-  Wire.write(0x10);                   // Set the register bits as 00010000 (1000dps full scale)
+  Wire.write(0x1B); // Talk to the GYRO_CONFIG register (1B hex)
+  Wire.write(0x10); // Set the register bits as 00010000 (1000dps full scale)
   Wire.endTransmission(true);
 }
 
-void calculate_IMU_error() {
+void calculate_IMU_error()
+{
   // We can call this funtion in the setup section to calculate the accelerometer and gury data error. From here we will get the error values used in the above equations printed on the Serial Monitor.
   // Note that we should place the IMU flat in order to get the proper values, so that we then can the correct values
   // Read accelerometer values 200 times
-  while (c < 200) {
+  while (c < 200)
+  {
     Wire.beginTransmission(MPU);
     Wire.write(0x3B);
     Wire.endTransmission(false);
     Wire.requestFrom(MPU, 6, true);
-    AccX = (Wire.read() << 8 | Wire.read()) / 4096.0 ;
-    AccY = (Wire.read() << 8 | Wire.read()) / 4096.0 ;
-    AccZ = (Wire.read() << 8 | Wire.read()) / 4096.0 ;
+    AccX = (Wire.read() << 8 | Wire.read()) / 4096.0;
+    AccY = (Wire.read() << 8 | Wire.read()) / 4096.0;
+    AccZ = (Wire.read() << 8 | Wire.read()) / 4096.0;
     // Sum all readings
     AccErrorX = AccErrorX + ((atan((AccY) / sqrt(pow((AccX), 2) + pow((AccZ), 2))) * 180 / PI));
     AccErrorY = AccErrorY + ((atan(-1 * (AccX) / sqrt(pow((AccY), 2) + pow((AccZ), 2))) * 180 / PI));
@@ -152,7 +186,8 @@ void calculate_IMU_error() {
   AccErrorY = AccErrorY / 200;
   c = 0;
   // Read gyro values 200 times
-  while (c < 200) {
+  while (c < 200)
+  {
     Wire.beginTransmission(MPU);
     Wire.write(0x43);
     Wire.endTransmission(false);
@@ -178,7 +213,8 @@ void calculate_IMU_error() {
   Serial.println(GyroErrorY);
 }
 
-void read_IMU() {
+void read_IMU()
+{
   // === Read acceleromter data === //
   Wire.beginTransmission(MPU);
   Wire.write(0x3B); // Start with register 0x3B (ACCEL_XOUT_H)
@@ -190,16 +226,16 @@ void read_IMU() {
   AccY = (Wire.read() << 8 | Wire.read()) / 4096.0; // Y-axis value
   AccZ = (Wire.read() << 8 | Wire.read()) / 4096.0; // Z-axis value
   // Calculating angle values using
-  accAngleX = (atan(AccY / sqrt(pow(AccX, 2) + pow(AccZ, 2))) * 180 / PI) + 1.15; // AccErrorX ~(-1.15) See the calculate_IMU_error()custom function for more details
+  accAngleX = (atan(AccY / sqrt(pow(AccX, 2) + pow(AccZ, 2))) * 180 / PI) + 1.15;      // AccErrorX ~(-1.15) See the calculate_IMU_error()custom function for more details
   accAngleY = (atan(-1 * AccX / sqrt(pow(AccY, 2) + pow(AccZ, 2))) * 180 / PI) - 0.52; // AccErrorX ~(0.5)
   // === Read gyro data === //
-  previousTime = currentTime;        // Previous time is stored before the actual time read
-  currentTime = millis();            // Current time actual time read
-  elapsedTime = (currentTime - previousTime) / 1000;   // Divide by 1000 to get seconds
+  previousTime = currentTime;                        // Previous time is stored before the actual time read
+  currentTime = millis();                            // Current time actual time read
+  elapsedTime = (currentTime - previousTime) / 1000; // Divide by 1000 to get seconds
   Wire.beginTransmission(MPU);
   Wire.write(0x43); // Gyro data first register address 0x43
   Wire.endTransmission(false);
-  Wire.requestFrom(MPU, 4, true); // Read 4 registers total, each axis value is stored in 2 registers
+  Wire.requestFrom(MPU, 4, true);                  // Read 4 registers total, each axis value is stored in 2 registers
   GyroX = (Wire.read() << 8 | Wire.read()) / 32.8; // For a 1000dps range we have to divide first the raw value by 32.8, according to the datasheet
   GyroY = (Wire.read() << 8 | Wire.read()) / 32.8;
   GyroX = GyroX + 1.85; //// GyroErrorX ~(-1.85)
@@ -214,4 +250,3 @@ void read_IMU() {
   data.roll = map(angleX, -90, +90, 255, 0);
   data.pitch = map(angleY, -90, +90, 0, 255);
 }
-
