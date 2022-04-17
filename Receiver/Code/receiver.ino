c9c5f4b3ccc71e7de35b500404790c3215f86ee4
Mohanad <abualhan45@gmail.com>
2022-04-17 14:33:49 +0200

    using YMFC-AL flight controller instead of Multiwii

--- Receiver/Code/receiver.ino
+++ Receiver/Code/receiver.ino
@@ -1,87 +1,75 @@
-// Don't forget to change the radio pin according to the schematic
+
+// Receiver code for the Arduino Radio control with PWM output
+// This code receive 6 channels and create a PWM output for each one on D2, D3, D4, D5, D6 and D7
+
+
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-#include <stdint.h>
+#include <Servo.h>
 
-////////////////////// PPM CONFIGURATION//////////////////////////
-#define channel_number 6 //set the number of channels
-#define sigPin 2         //set PPM signal output pin on the arduino
-#define PPM_FrLen 27000  //set the PPM frame length in microseconds (1ms = 1000µs)
-#define PPM_PulseLen 400 //set the pulse length
-//////////////////////////////////////////////////////////////////
+//Define widths
+int pwm_width_2 = 0;
+int pwm_width_3 = 0;
+int pwm_width_4 = 0;
+int pwm_width_5 = 0;
+int pwm_width_6 = 0;
+int pwm_width_7 = 0;
 
-int ppm[channel_number];
+Servo PWM2;
+Servo PWM3;
+Servo PWM4;
+Servo PWM5;
+Servo PWM6;
+Servo PWM7;
 
-const uint64_t pipeIn = 0xE8E8F0F0E1LL;
 
-RF24 radio(9, 10); // (CE, CSN)
-
-// The sizeof this struct should not exceed 32 bytes
-struct MyData
-{
-  byte throttle;
-  byte yaw;
-  byte pitch;
-  byte roll;
-  byte AUX1;
-  byte AUX2;
+struct MyData {
+byte throttle;      
+byte yaw;
+byte pitch;
+byte roll;
+byte AUX1;
+byte AUX2;
 };
-
 MyData data;
+
+
+const uint64_t pipeIn = 0xE8E8F0F0E1LL;     //same as in the transmitter
+RF24 radio(9, 10); 
+
+
 
 void resetData()
 {
-  // 'safe' values to use when no radio input is detected
-  data.throttle = 0;
-  data.yaw = 127;
-  data.pitch = 127;
-  data.roll = 127;
-  data.AUX1 = 0;
-  data.AUX2 = 0;
-
-  setPPMValuesFromData();
-}
-
-void setPPMValuesFromData()
-{
-  ppm[0] = map(data.throttle, 0, 255, 1000, 2000);
-  ppm[1] = map(data.yaw, 0, 255, 1000, 2000);
-  ppm[2] = map(data.pitch, 0, 255, 1000, 2000);
-  ppm[3] = map(data.roll, 0, 255, 1000, 2000);
-  ppm[4] = map(data.AUX1, 0, 1, 1000, 2000);
-  ppm[5] = map(data.AUX2, 0, 1, 1000, 2000);
+data.throttle = 0;
+data.yaw = 127;
+data.pitch = 127;
+data.roll = 127;
+data.AUX1 = 0;
+data.AUX2 = 0;
 }
 
 /**************************************************/
 
-void setupPPM()
-{
-  pinMode(sigPin, OUTPUT);
-  digitalWrite(sigPin, 0); //set the PPM signal pin to the default state (off)
-
-  cli();
-  TCCR1A = 0; // set entire TCCR1 register to 0
-  TCCR1B = 0;
-
-  OCR1A = 100;             // compare match register (not very important, sets the timeout for the first interrupt)
-  TCCR1B |= (1 << WGM12);  // turn on CTC mode
-  TCCR1B |= (1 << CS11);   // 8 prescaler: 0,5 microseconds at 16mhz
-  TIMSK1 |= (1 << OCIE1A); // enable timer compare interrupt
-  sei();
-}
-
 void setup()
 {
+  //Set the pins for each PWM signal
+  PWM2.attach(2);
+  PWM3.attach(3);
+  PWM4.attach(4);
+  PWM5.attach(5);
+  PWM6.attach(6);
+  PWM7.attach(7);
+
+  //Configure the NRF24 module
   resetData();
-  setupPPM();
-
-  // Set up radio module
   radio.begin();
-  radio.setDataRate(RF24_250KBPS); // Both endpoints must have this set the same
   radio.setAutoAck(false);
-
-  radio.openReadingPipe(1, pipeIn);
+  radio.setDataRate(RF24_250KBPS);  
+  radio.openReadingPipe(1,pipeIn);
+  
+  //we start the radio comunication
   radio.startListening();
 }
 
@@ -91,68 +79,39 @@ unsigned long lastRecvTime = 0;
 
 void recvData()
 {
-  while (radio.available())
-  {
-    radio.read(&data, sizeof(MyData));
-    lastRecvTime = millis();
-  }
+while ( radio.available() ) {
+radio.read(&data, sizeof(MyData));
+lastRecvTime = millis(); //here we receive the data
+}
 }
 
 /**************************************************/
 
 void loop()
 {
-  recvData();
-
-  unsigned long now = millis();
-  if (now - lastRecvTime > 1000)
-  {
-    // signal lost?
-    resetData();
-  }
-
-  setPPMValuesFromData();
+recvData();
+unsigned long now = millis();
+// check if we've lost signal, if we did we reset the values 
+if ( now - lastRecvTime > 1000 ) {
+// Signal lost?
+resetData();
 }
 
-/**************************************************/
+pwm_width_2 = map(data.throttle, 0, 255, 1000, 2000);     //PWM value on digital pin D2
+pwm_width_3 = map(data.yaw,      0, 255, 1000, 2000);     //PWM value on digital pin D3
+pwm_width_4 = map(data.pitch,    0, 255, 1000, 2000);     //PWM value on digital pin D4
+pwm_width_5 = map(data.roll,     0, 255, 1000, 2000);     //PWM value on digital pin D5
+pwm_width_6 = map(data.AUX1,     0, 255, 1000, 2000);     //PWM value on digital pin D6
+pwm_width_7 = map(data.AUX2,     0, 255, 1000, 2000);     //PWM value on digital pin D7
 
-//#error Delete this line befor you cahnge the value (clockMultiplier) below
-#define clockMultiplier 2 // set this to 2 if you are using a 16MHz arduino, leave as 1 for an 8MHz arduino
 
-ISR(TIMER1_COMPA_vect)
-{
-  static boolean state = true;
+// write the PWM signal using the servo function
+PWM2.writeMicroseconds(pwm_width_2);
+PWM3.writeMicroseconds(pwm_width_3);
+PWM4.writeMicroseconds(pwm_width_4);
+PWM5.writeMicroseconds(pwm_width_5);
+PWM6.writeMicroseconds(pwm_width_6);
+PWM7.writeMicroseconds(pwm_width_7);
 
-  TCNT1 = 0;
 
-  if (state)
-  {
-    //end pulse
-    PORTD = PORTD & ~B00000100; // turn pin 2 off. Could also use: digitalWrite(sigPin,0)
-    OCR1A = PPM_PulseLen * clockMultiplier;
-    state = false;
-  }
-  else
-  {
-    //start pulse
-    static byte cur_chan_numb;
-    static unsigned int calc_rest;
-
-    PORTD = PORTD | B00000100; // turn pin 2 on. Could also use: digitalWrite(sigPin,1)
-    state = true;
-
-    if (cur_chan_numb >= channel_number)
-    {
-      cur_chan_numb = 0;
-      calc_rest += PPM_PulseLen;
-      OCR1A = (PPM_FrLen - calc_rest) * clockMultiplier;
-      calc_rest = 0;
-    }
-    else
-    {
-      OCR1A = (ppm[cur_chan_numb] - PPM_PulseLen) * clockMultiplier;
-      calc_rest += ppm[cur_chan_numb];
-      cur_chan_numb++;
-    }
-  }
 }
